9ec94696aa78f1ffdc096a70f513d59e2506a043
Trevor Johnson <trevorjohnson9601@gmail.com>
2022-07-17 10:02:46 -0400

    Get hard and unbeatable AI working

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ const gameBoard = (() => {
         cellClassList = Array.from(cellClassList);
         yCoord = parseInt(cellClassList[2].split("-")[1]);
         return yCoord;
-    }   
+    }
     const makeNewGame = (readyState) => {
         xTurn = true;
         const winnerDeclaration = document.querySelector(".winner-declaration");
@@ -52,11 +52,11 @@ const gameBoard = (() => {
         gameBoardArray[y - 1][x - 1] = value;
         let cellToChange = document.querySelector(`.column-${x}.row-${y}`);
         cellToChange.textContent = value;
-        checkGameEnd(x, y, gameBoardArray);
+        checkGameEnd(x, y);
         xTurn = !xTurn;
         if (compSide === getTurn() && document.querySelector(".winner-declaration").textContent === "") { controlComputer() };
     }
-    const checkGameEnd = (x, y, gameBoardArray) => {
+    const checkGameEnd = (x, y) => {
         // check for vertical wins  
         if (gameBoardArray[(y - 1) % 3][x - 1] === gameBoardArray[y % 3][x - 1] && gameBoardArray[y % 3][x - 1] === gameBoardArray[(y + 1) % 3][x - 1]) {
             declareWinner(gameBoardArray[y - 1][x - 1]);
@@ -76,6 +76,17 @@ const gameBoard = (() => {
                 }
             }
         }
+        for (let i = 0; i < 3; i++) {
+            for (let j = 0; j < 3; j++) {
+                if (gameBoardArray[i][j] === "") {
+                    return;
+                }
+            }
+        }
+        const winnerDeclaration = document.querySelector(".winner-declaration");
+        winnerDeclaration.textContent = "It's a tie!";
+        endGame();
+
     }
     const declareWinner = (winner) => {
         const winnerDeclaration = document.querySelector(".winner-declaration");
@@ -111,6 +122,12 @@ const gameBoard = (() => {
         switch (difficulty) {
             case "easy":
                 aiController.easyAI();
+                break;
+            case "hard":
+                aiController.hardAI();
+                break;
+            case "unbeatable":
+                aiController.findBestMove();
         }
         boardCells = document.querySelectorAll(".cell");
         setTimeout(function () {
@@ -157,13 +174,16 @@ const gameSettings = (() => {
             difficultySelect.classList.add("difficulty-select");
             let easyOption = document.createElement("option");
             let hardOption = document.createElement("option");
+            let unbeatableOption = document.createElement("option");     
             let xOption = document.createElement("button");
             let oOption = document.createElement("button");
             let startButton = document.createElement("button");
             easyOption.setAttribute("value", "easy");
             hardOption.setAttribute("value", "hard");
+            unbeatableOption.setAttribute("value", "unbeatable");
             easyOption.textContent = "Easy";
             hardOption.textContent = "Hard";
+            unbeatableOption.textContent = "Unbeatable";
             xOption.classList.add("x", "side-select-button", "disabled");
             oOption.classList.add("o", "side-select-button");
             xOption.textContent = "X";
@@ -173,7 +193,7 @@ const gameSettings = (() => {
             startButton.setAttribute("type", "button");
             startButton.classList.add("start-button");
             startButton.textContent = "Start Game";
-            difficultySelect.append(easyOption, hardOption);
+            difficultySelect.append(easyOption, hardOption, unbeatableOption);
             playerSelectArea.appendChild(difficultySelect);
             sideSelectArea.append(xOption, oOption);
             gameBoard.appendChild(startButton);
@@ -197,51 +217,135 @@ const gameSettings = (() => {
     }
     return { setPlayerSelect, toggleSide };
 })();
-const aiController = (()=>{
-    const evaluateEnd = () =>{
-        // check for vertical wins  
-        if (gameBoardArray[(y - 1) % 3][x - 1] === gameBoardArray[y % 3][x - 1] && gameBoardArray[y % 3][x - 1] === gameBoardArray[(y + 1) % 3][x - 1]) {
-            return compSide === gameBoardArray[y % 3][x - 1] ? 10 : -10;
-        }
-        // check for horizontal wins
-        else if ((gameBoardArray[y - 1][(x - 1) % 3] === gameBoardArray[y - 1][x % 3] && gameBoardArray[y - 1][x % 3] === gameBoardArray[y - 1][(x + 1) % 3])) {
-            return compSide === gameBoardArray[y - 1][(x - 1) % 3] ? 10 : -10;
-        }
-        // check for diagonal wins
-        else if ((x + y) % 2 === 0) {
-            if (x === y && ((gameBoardArray[(y - 1) % 3][(x - 1) % 3] === gameBoardArray[y % 3][x % 3] && gameBoardArray[y % 3][x % 3] === gameBoardArray[(y + 1) % 3][(x + 1) % 3]))) {
-                return compSide === gameBoardArray[(y - 1) % 3][(x - 1) % 3] ? 10 : -10;
+const aiController = (() => {
+    const evaluateEnd = () => {
+
+        for (let row = 0; row < 3; row++) {
+            if (gameBoardArray[row][0] === gameBoardArray[row][1] &&
+                gameBoardArray[row][1] === gameBoardArray[row][2]) {
+                if (gameBoardArray[row][0] === compSide)
+                    return +10;
+
+                else if (gameBoardArray[row][0] === playerSide)
+                    return -10;
             }
-            else if (x !== y || x === 2) {
-                if (gameBoardArray[2][0] === gameBoardArray[1][1] && gameBoardArray[1][1] === gameBoardArray[0][2]) {
-                    return compSide === gameBoardArray[2][0] === gameBoardArray[1][1] ? 10 : -10;
-                }
+        }
+
+        for (let col = 0; col < 3; col++) {
+            if (gameBoardArray[0][col] === gameBoardArray[1][col] &&
+                gameBoardArray[1][col] === gameBoardArray[2][col]) {
+                if (gameBoardArray[0][col] === compSide)
+                    return +10;
+
+                else if (gameBoardArray[0][col] === playerSide)
+                    return -10;
             }
+        }
+        if (gameBoardArray[0][0] === gameBoardArray[1][1] && gameBoardArray[1][1] === gameBoardArray[2][2]) {
+            if (gameBoardArray[0][0] === compSide)
+                return +10;
+
+            else if (gameBoardArray[0][0] === playerSide)
+                return -10;
+        }
+        if (gameBoardArray[0][2] === gameBoardArray[1][1] &&
+            gameBoardArray[1][1] === gameBoardArray[2][0]) {
+            if (gameBoardArray[0][2] === compSide)
+                return +10;
+
+            else if (gameBoardArray[0][2] === playerSide)
+                return -10;
         }
         return 0;
     }
-    const isMovesLeft = ()=>{
-        for(let i = 0; i<3; i++){
-            for(let j = 0; j<3; j++){
-                if (!(gameBoardArray[i][j])){
+    const isMovesLeft = () => {
+        for (let i = 0; i < 3; i++) {
+            for (let j = 0; j < 3; j++) {
+                if (!(gameBoardArray[i][j])) {
                     return true;
                 }
             }
         }
         return false;
     }
+    // Adapted from geeks for geeks minimax tutorial found here https://www.geeksforgeeks.org/minimax-algorithm-in-game-theory-set-3-tic-tac-toe-ai-finding-optimal-move/
+    const minimax = (depth, isMax) => {
+        let score = evaluateEnd();
+        if (score === 10) {
+            return (score - depth);
+        }
+        if (score === -10) {
+            return (score + depth)
+        }
+        if (!isMovesLeft()) {
+            return 0;
+        }
+        if (isMax) {
+            let best = -1000;
+            for (let i = 0; i < 3; i++) {
+                for (let j = 0; j < 3; j++) {
+                    if (gameBoardArray[i][j] === '') {
+                        gameBoardArray[i][j] = compSide;
+                        best = Math.max(best, minimax(depth + 1, !isMax));
+                        gameBoardArray[i][j] = '';
+                    }
+                }
+            }
+            return best;
+        }
+        else {
+            let best = 1000;
+            for (let i = 0; i < 3; i++) {
+                for (let j = 0; j < 3; j++) {
+                    if (gameBoardArray[i][j] === '') {
+                        gameBoardArray[i][j] = playerSide;
+                        best = Math.min(best, minimax(depth + 1, !isMax));
+                        gameBoardArray[i][j] = '';
+                    }
+                }
+            }
+            return best;
+        }
+    }
+    const findBestMove = () => {
+        let bestVal = -1000;
+        let bestMove = [-1, -1];
+        for (let i = 0; i < 3; i++) {
+            for (let j = 0; j < 3; j++) {
+                if (gameBoardArray[i][j] === '') {
+                    gameBoardArray[i][j] = compSide;
+                    let moveVal = minimax(0, false);
+                    gameBoardArray[i][j] = '';
+                    if (moveVal > bestVal) {
+                        bestMove[1] = i;
+                        bestMove[0] = j;
+                        bestVal = moveVal;
+                    }
+                }
+            }
+        }
+        setTimeout(function () { gameBoard.updateBoard(bestMove[0] + 1, bestMove[1] + 1, compSide) }, 1000);
+    }
+    const hardAI = ()=>{
+        randomNum = Math.floor(Math.random()*100);
+        if(randomNum<25){
+            easyAI();
+        }
+        else{
+            findBestMove();
+        }
+    }
     const easyAI = () => {
         const value = compSide;
         while (true) {
             xCoord = Math.floor(Math.random() * 3) + 1;
             yCoord = Math.floor(Math.random() * 3) + 1;
-            if (gameBoardArray[yCoord - 1][xCoord - 1]){continue};
+            if (gameBoardArray[yCoord - 1][xCoord - 1]) { continue };
             setTimeout(function () { gameBoard.updateBoard(xCoord, yCoord, value) }, 1000);
             return;
-
         }
     }
-    return {easyAI};
+    return { easyAI, hardAI, findBestMove };
 })();
 gameBoard.makeNewGame(true);
 gameSettings.setPlayerSelect();
